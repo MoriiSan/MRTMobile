4db00dc432870bcc2dfede47a2806eb1c13c5afc
Moriartti <92302869+MoriiSan@users.noreply.github.com>
2024-02-27 23:57:02 +0800

    update pin setup

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import type { PropsWithChildren } from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {
   SafeAreaView,
   ScrollView,
@@ -13,11 +13,60 @@ import {
   TouchableOpacity
 } from 'react-native';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
+  const [pin1, setPin1] = React.useState('');
+  const [pin2, setPin2] = React.useState('');
+  const [pin3, setPin3] = React.useState('');
+  const [pin4, setPin4] = React.useState('');
+  const pin1Ref = React.useRef<TextInput>(null);
+  const pin2Ref = React.useRef<TextInput>(null);
+  const pin3Ref = React.useRef<TextInput>(null);
+  const pin4Ref = React.useRef<TextInput>(null);
+
+  const handlePin1Change = (text: string) => {
+    const filteredText = text.replace(/[^0-9]/g, ''); // Filter out non-numeric characters
+    setPin1(filteredText);
+    if (filteredText === '') {
+      pin1Ref.current?.focus();
+    } else {
+      pin2Ref.current?.focus();
+    }
+  };
+
+  const handlePin2Change = (text: string) => {
+    const filteredText = text.replace(/[^0-9]/g, ''); // Filter out non-numeric characters
+    setPin2(filteredText);
+    if (filteredText === '') {
+      pin1Ref.current?.focus();
+    } else {
+      pin3Ref.current?.focus();
+    }
+  };
+
+  const handlePin3Change = (text: string) => {
+    const filteredText = text.replace(/[^0-9]/g, ''); // Filter out non-numeric characters
+    setPin3(filteredText);
+    if (filteredText === '') {
+      pin2Ref.current?.focus();
+    } else {
+      pin4Ref.current?.focus();
+    }
+  };
+
+  const handlePin4Change = (text: string) => {
+  const filteredText = text.replace(/[^0-9]/g, '');
+  setPin4(filteredText);
+  if (filteredText === '') {
+    pin3Ref.current?.focus();
+  } else {
+    pin4Ref.current?.focus();
+  }
+};
+
+
   return (
     <NavigationContainer>
       <SafeAreaView style={styles.container}>
@@ -32,28 +81,40 @@ function App(): React.JSX.Element {
             </Text>
             <View style={styles.pinInputContainer}>
               <TextInput
+                ref={pin1Ref}
                 style={styles.pinInput}
                 keyboardType="numeric"
                 maxLength={1}
                 secureTextEntry={true}
+                onChangeText={handlePin1Change}
+                value={pin1}
               />
               <TextInput
+                ref={pin2Ref}
                 style={styles.pinInput}
                 keyboardType="numeric"
                 maxLength={1}
                 secureTextEntry={true}
+                onChangeText={handlePin2Change}
+                value={pin2}
               />
               <TextInput
+                ref={pin3Ref}
                 style={styles.pinInput}
                 keyboardType="numeric"
                 maxLength={1}
                 secureTextEntry={true}
+                onChangeText={handlePin3Change}
+                value={pin3}
               />
               <TextInput
+                ref={pin4Ref}
                 style={styles.pinInput}
                 keyboardType="numeric"
                 maxLength={1}
                 secureTextEntry={true}
+                onChangeText={handlePin4Change}
+                value={pin4}
               />
             </View>
             <TouchableOpacity
@@ -62,7 +123,8 @@ function App(): React.JSX.Element {
             >
               <Text
                 style={styles.submitText}
-              >Submit</Text>
+              >Submit
+              </Text>
             </TouchableOpacity>
           </View>
         </ScrollView>
@@ -81,18 +143,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 20,
+    marginTop: 200,
   },
   pinTitle: {
     fontSize: 24,
     fontWeight: '900',
     color: 'black',
-    marginBottom: 10,
   },
   pinDescr: {
     fontSize: 16,
     fontWeight: '400',
     color: '#888',
-    marginBottom: 20,
+    marginBottom: 25,
   },
   pinInputContainer: {
     flexDirection: 'row',
@@ -111,12 +173,13 @@ const styles = StyleSheet.create({
   submitBtn: {
     marginTop: 10,
     paddingVertical: 10,
-    paddingHorizontal: 20,
+    paddingHorizontal: 75,
     borderRadius: 5,
     backgroundColor: '#ffb301',
   },
   submitText: {
     fontWeight: '900',
+    color: 'white',
   }
 });
 
